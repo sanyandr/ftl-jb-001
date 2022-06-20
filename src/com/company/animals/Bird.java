2e4129205f4a28899f1162627a6b3b9231ba6022
Sanyandr <alexenderboot@gmail.com>
2022-06-20 13:49:53 +0300

    L007 - added duck & bird

---
+++ src/com/company/animals/Bird.java
@@ -0,0 +1,20 @@
+package com.company.animals;
+
+public class Bird extends Animal {
+    protected Boolean flyModeActive = false;
+
+
+    public void takeOff() {
+        flyModeActive = true;
+    }
+
+    public void landing() {
+        flyModeActive = false;
+    }
+
+    public void isTheBirdFlying () {
+        System.out.println(
+                this.flyModeActive == true ? "Duck is flying" : "Duck isn't flying"
+        );
+    }
+}
