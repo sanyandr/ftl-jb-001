5c770696d53f785e1572c60832ef3e8f5e6d1ae2
Sanyandr <alexenderboot@gmail.com>
2022-07-15 00:42:49 +0300

    TASK007 - stars

---
+++ src/com/company/T007/StarsMatrix.java
@@ -0,0 +1,13 @@
+package com.company.T007;
+
+public class StarsMatrix {
+    public static void run() {
+        char star = '*';
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(star + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
