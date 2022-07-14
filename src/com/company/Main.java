a0cd4faa6175e3e6686c198b1423c0926f6e44a5
Sanyandr <alexenderboot@gmail.com>
2022-07-14 22:32:06 +0300

    TASK002 - reverse output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,17 @@ package com.company;
 
 import com.company.L001.HelloWorld;
 import com.company.T001.OutputNumbers;
+import com.company.T002.OutputAnotherNumbers;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("========TASKS========");
+        System.out.println("TASK001");
         OutputNumbers.run();
         System.out.println("\n ");
+        System.out.println("TASK002");
+        OutputAnotherNumbers.run();
+        System.out.println("\n ");
+
     }
 }
